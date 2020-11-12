7d9db0330ec35609ed2ca673d2870968b07e0f60
Tristan_Briseno <tristan.k.briseno@gmail.com>
2020-11-12 14:18:05 -0800

    first commit

---
+++ functions.py
@@ -0,0 +1,27 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+result_2 = area(3, 4)
+result_3 = area(2, 8)
+print(result)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+result = subtract(10,5)
+result_2 = subtract(9,3)
+print(result, result_2)
+
+def divide(num1, num2)
+    result = num1 / num2
+    return result
+result = divide(8, 2)
+print(result)
+
+def add(num1, num2):
+    result = num1 - num2
+    return result
+result = add(7,5)
+result_2 = add(12,3)
+print(result, result_2)
